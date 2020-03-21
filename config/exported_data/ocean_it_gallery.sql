f35dad98df7654540e26ef989a0830288d4e262a
uitlab421s <uitlab421s@gmail.com>
2020-03-21 14:32:53 +0900

    save

--- config/exported_data/ocean_it_gallery.sql
+++ config/exported_data/ocean_it_gallery.sql
@@ -7,13 +7,12 @@ CREATE TABLE `gallery` (
   `gid` int(11) NOT NULL AUTO_INCREMENT,
   `title_ko` varchar(100) NOT NULL,
   `title_en` varchar(100) NOT NULL,
-  `image_size` int(11) NOT NULL,
   `image_src` varchar(100) NOT NULL, /*이미지 고정 : 가로 960px, 세로 480px*/
   PRIMARY KEY (`gid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
-INSERT INTO `gallery` VALUES (1,'제목이 들어갑니다','a 들어갑니다','1','pic00.jpg');
-INSERT INTO `gallery` VALUES (2,'제목이 들어갑니다','a 들어갑니다','3','pic01.jpg/pic02.jpg/pic00.jpg');
-INSERT INTO `gallery` VALUES (3,'제목이 들어갑니다','a 들어갑니다','2','pic02.jpg/pic04.jpg');
-INSERT INTO `gallery` VALUES (4,'제목이 들어갑니다','a 들어갑니다','3','pic03.jpg/pic06.jpg/pic02.jpg');
+INSERT INTO `gallery` VALUES (1,'제목이 들어갑니다','a 들어갑니다','pic00.jpg');
+INSERT INTO `gallery` VALUES (2,'제목이 들어갑니다','a 들어갑니다','pic01.jpg/pic02.jpg/pic00.jpg');
+INSERT INTO `gallery` VALUES (3,'제목이 들어갑니다','a 들어갑니다','pic02.jpg/pic04.jpg');
+INSERT INTO `gallery` VALUES (4,'제목이 들어갑니다','a 들어갑니다','pic03.jpg/pic06.jpg/pic02.jpg');
