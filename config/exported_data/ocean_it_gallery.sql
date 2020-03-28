9911547116b9e305dfd201ffd2ab73467c6f6931
uitlab421s <uitlab421s@gmail.com>
2020-03-28 14:17:54 +0900

    change sql fields

--- config/exported_data/ocean_it_gallery.sql
+++ config/exported_data/ocean_it_gallery.sql
@@ -5,14 +5,30 @@ DROP TABLE IF EXISTS `gallery`;
 SET character_set_client = utf8mb4 ;
 CREATE TABLE `gallery` (
   `gid` int(11) NOT NULL AUTO_INCREMENT,
-  `title_ko` varchar(100) NOT NULL,
-  `title_en` varchar(100) NOT NULL,
-  `image_src` varchar(100) NOT NULL, /*이미지 고정 : 가로 960px, 세로 480px*/
+  `title_ko` varchar(300) NOT NULL,
+  `title_en` varchar(300) NOT NULL,
+  `image_src` varchar(300) NOT NULL, /*이미지 고정 : 가로 960px, 세로 480px*/
   PRIMARY KEY (`gid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
-INSERT INTO `gallery` VALUES (1,'제목이 들어갑니다','a 들어갑니다','pic00.jpg');
-INSERT INTO `gallery` VALUES (2,'제목이 들어갑니다','a 들어갑니다','pic01.jpg/pic02.jpg/pic00.jpg');
-INSERT INTO `gallery` VALUES (3,'제목이 들어갑니다','a 들어갑니다','pic02.jpg/pic04.jpg');
-INSERT INTO `gallery` VALUES (4,'제목이 들어갑니다','a 들어갑니다','pic03.jpg/pic06.jpg/pic02.jpg');
+INSERT INTO `gallery` VALUES (1,'2019.11.잠수함 과학기술 발전 세미나','2019.11.Submarine Science and Technology Development Seminar','메인사진1.jpg');
+INSERT INTO `gallery` VALUES (2,'2019.10. 초연결·고신뢰 산업지능 기술 세미나','2019.10.Connected and highly reliable industrial intelligence technology seminar in early','메인사진2.jpg/2.jpg/3.jpg/4.jpg/5.jpg');
+INSERT INTO `gallery` VALUES (3,'2018.10. 제82차 국제전기기술위원회 총회 2018 IEC 부산총회','2018.10. 82nd International Electrotechnical Commission General Assembly 2018 IEC Busan General Assembly','메인사진3.jpg/6.jpg/7.jpg/8.jpg/9.jpg/10.jpg/11.jpg/12.jpg/13.jpg/14.jpg/15.jpg');
+INSERT INTO `gallery` VALUES (4,'2018.3 2018년도 해양IT융합기술연구소 워크샵','2018.3. 2018 Marine IT Convergence Technology Research Institute Workshop','메인사진4.jpg/16.jpg/17.jpg/18.jpg/19.jpg/20.jpg');
+INSERT INTO `gallery` VALUES (5,'2018.1 2018년도 호서대학교 해양IT융합기술연구소 워크샵','2018.1 2018Hoseo University Marine IT Convergence Technology Research Institute Workshop','메인사진5.jpg/21.jpg/22.jpg/23.jpg/24.jpg/25.jpg');
+INSERT INTO `gallery` VALUES (6,'2017.12. 제1회 해양수산 전문가대회','2017.12. The 1st Marine Fisheries Expert Conference','메인사진6.jpg/26.jpg/27.jpg');
+INSERT INTO `gallery` VALUES (7,'2017.11. 에너지기술평가원 이성주 실장 초청 세미나','2017.11. Invited Seminar by Director Seong-Ju Lee, Korea Energy Technology Institute','메인사진7.jpg/28.jpg/29.jpg');
+INSERT INTO `gallery` VALUES (8,'2017.4. ‘4차 산업혁명 시대, 수중통신 기술의 현재와 미래’세미나 개최','2017.4. Seminar held on “The 4th Industrial Revolution, Present and Future of Underwater Communication Technology”','메인사진8.jpg/30.jpg/31.jpg/32.jpg/33.jpg/34.jpg');
+INSERT INTO `gallery` VALUES (9,'2017.2. 충청남도-호서대학교 업무협약식',' 2017.2. Chungcheongnam-do-Hoseo University Business Agreement','메인사진9.jpg/35.jpg/36.jpg');
+INSERT INTO `gallery` VALUES (10,'2016.3. 제3회 해양과학기술정책 포럼',' 2016.3. The 3rd Marine Science and Technology Policy Forum','메인사진10.jpg/37.jpg/38.jpg/39.jpg/40.jpg/41.jpg');
+INSERT INTO `gallery` VALUES (11,'2015.6. 제2회 해양과학기술정책 포럼',' 2015.6. The 2nd Marine Science and Technology Policy Forum','메인사진11.jpg/42.jpg');
+INSERT INTO `gallery` VALUES (12,'2014.12. 제1회 해양과학기술정책 포럼',' 2014.12. 1st Marine Science and Technology Policy Forum','메인사진12.jpg/43.jpg/44.jpg/45.jpg/46.jpg');
+
+
+
+
+
+
+
+
--- config/exported_data/ocean_it_members.sql
+++ config/exported_data/ocean_it_members.sql
@@ -23,35 +23,36 @@ INSERT INTO `members` VALUES (6,'김민상','Min-Sang Kim','해양IT융합기술
 INSERT INTO `members` VALUES (7,'차민혁','Min-Hyeok Cha','해양IT융합기술연구소','전임연구교수 및 전임연구원','041-540-9565','dwmh86@hoseo.edu','차민혁.jpg');
 INSERT INTO `members` VALUES (8,'난디만달람모함','Mohan Krishna Varma Nandimandalam','해양IT융합기술연구소','전임연구교수 및 전임연구원','041-540-9565','drmohankvn@hoseo.edu','난디만달람모함.jpg');
 INSERT INTO `members` VALUES (9,'이효찬','Hyo-Chan Lee','해양IT융합기술연구소','전임연구교수 및 전임연구원','041-540-9565','lhc_104@naver.com','이효찬.jpg');
-INSERT INTO `members` VALUES (10,'김세연','Se-Yeon Kim','해양IT융합기술연구소','전임연구교수 및 전임연구원','041-540-9565','seyeon92@hoseo.edu','김세연.jpg');
+INSERT INTO `members` VALUES (10,'칸 티 투 투이','CAN THI THU THUY','해양IT융합기술연구소','연구원','041-540-5965','kangchiwa@gmail.com','칸티투투이.jpg');
+INSERT INTO `members` VALUES (11,'김세연','Se-Yeon Kim','해양IT융합기술연구소','전임연구교수 및 전임연구원','041-540-9565','seyeon92@hoseo.edu','김세연.jpg');
 
 
 
-INSERT INTO `members` VALUES (11,'김준호','Joonho Kim','정보통신공학','박사과정','-','katsurablue@naver.com','김준호.jpg'); 
-INSERT INTO `members` VALUES (12,'정태건','Tae Geon Jeong','정보통신공학','박사과정','-','a40402@naver.com','정태건.jpg'); 
-INSERT INTO `members` VALUES (13,'채광영','Chae Gwang-young','정보통신공학','박사과정','-','rhkddud0822@naver.com','채광영.jpg'); 
-INSERT INTO `members` VALUES (14,'뤄징신','Luo Jingxin','정보통신공학','박사과정','-','binglin0716@163.com','뤄징신.jpg'); 
-INSERT INTO `members` VALUES (15,'류평','Liu Pyeong','정보통신공학','박사과정','-','kunliupeng@163.com','류평.jpg'); 
-INSERT INTO `members` VALUES (16,'쑨화쿠이','Shunhuakui','정보통신공학','박사과정','-','sunhuakui@163.com','쑨화쿠이.jpg'); 
-INSERT INTO `members` VALUES (17,'펑쥔잉','Pengjun Ying','정보통신공학','박사과정','-','zwpfjy@163.com','펑쥔잉.jpg'); 
+INSERT INTO `members` VALUES (12,'김준호','Joonho Kim','정보통신공학','박사과정','-','katsurablue@naver.com','김준호.jpg'); 
+INSERT INTO `members` VALUES (13,'정태건','Tae Geon Jeong','정보통신공학','박사과정','-','a40402@naver.com','정태건.jpg'); 
+INSERT INTO `members` VALUES (14,'채광영','Chae Gwang-young','정보통신공학','박사과정','-','rhkddud0822@naver.com','채광영.jpg'); 
+INSERT INTO `members` VALUES (15,'뤄징신','Luo Jingxin','정보통신공학','박사과정','-','binglin0716@163.com','뤄징신.jpg'); 
+INSERT INTO `members` VALUES (16,'류평','Liu Pyeong','정보통신공학','박사과정','-','kunliupeng@163.com','류평.jpg'); 
+INSERT INTO `members` VALUES (17,'쑨화쿠이','Shunhuakui','정보통신공학','박사과정','-','sunhuakui@163.com','쑨화쿠이.jpg'); 
+INSERT INTO `members` VALUES (18,'펑쥔잉','Pengjun Ying','정보통신공학','박사과정','-','zwpfjy@163.com','펑쥔잉.jpg'); 
 
 
-INSERT INTO `members` VALUES (18,'고만재','Ko Man-jae','정보통신공학','석사과정','-','akswoakstp2@naver.com','고만재.jpg');
-INSERT INTO `members` VALUES (19,'손세우','Son Sewoo','정보통신공학','석사과정','-','tpdn333@gmail.com','손세우.jpg'); 
-INSERT INTO `members` VALUES (20,'송현학','Song Hyunhak','정보통신공학','석사과정','-','rainy_930@naver.com','송현학.jpg'); 
-INSERT INTO `members` VALUES (21,'유양','Yooyang','정보통신공학','석사과정','-','ly960401@naver.com','유양.jpg'); 
-INSERT INTO `members` VALUES (22,'유호동','Yoo Ho Dong','정보통신공학','석사과정','-','liuhaodong1314@naver.com','유호동.jpg'); 
-INSERT INTO `members` VALUES (23,'이성주','Sungjoo Lee','정보통신공학','석사과정','-','sjlee3416@naver.com','이성주.jpg'); 
-INSERT INTO `members` VALUES (24,'전호석','Jeon Ho-Seok','정보통신공학','석사과정','-','wjsghtjr33@naver.com','전호석.jpg'); 
-INSERT INTO `members` VALUES (25,'조천치','Jocheonchi','정보통신공학','석사과정','-','zhaotianchi123@naver.com','조천치.jpg'); 
+INSERT INTO `members` VALUES (19,'고만재','Ko Man-jae','정보통신공학','석사과정','-','akswoakstp2@naver.com','고만재.jpg');
+INSERT INTO `members` VALUES (20,'손세우','Son Sewoo','정보통신공학','석사과정','-','tpdn333@gmail.com','손세우.jpg'); 
+INSERT INTO `members` VALUES (21,'송현학','Song Hyunhak','정보통신공학','석사과정','-','rainy_930@naver.com','송현학.jpg'); 
+INSERT INTO `members` VALUES (22,'유양','Yooyang','정보통신공학','석사과정','-','ly960401@naver.com','유양.jpg'); 
+INSERT INTO `members` VALUES (23,'유호동','Yoo Ho Dong','정보통신공학','석사과정','-','liuhaodong1314@naver.com','유호동.jpg'); 
+INSERT INTO `members` VALUES (24,'이성주','Sungjoo Lee','정보통신공학','석사과정','-','sjlee3416@naver.com','이성주.jpg'); 
+INSERT INTO `members` VALUES (25,'전호석','Jeon Ho-Seok','정보통신공학','석사과정','-','wjsghtjr33@naver.com','전호석.jpg'); 
+INSERT INTO `members` VALUES (26,'조천치','Jocheonchi','정보통신공학','석사과정','-','zhaotianchi123@naver.com','조천치.jpg'); 
 
 
-INSERT INTO `members` VALUES (26,'김도훈','Do-hoon Kim','해양융합기술공학','석사과정','-','ehgns320@naver.com','김도훈.jpg');
-INSERT INTO `members` VALUES (27,'정해지','Jeong Hae Ji','해양융합기술공학','석사과정','-','liuhaodong1314@naver.com','정해지.jpg'); 
-INSERT INTO `members` VALUES (28,'왕비','Wang Bee','해양융합기술공학','석사과정','-','wf765969609@gmail.com','왕비.jpg'); 
-INSERT INTO `members` VALUES (29,'장요','Jang yo','해양융합기술공학','석사과정','-','zhang5163365@gmail.com','장요.jpg'); 
-INSERT INTO `members` VALUES (30,'전원','Jun Won ','해양융합기술공학','석사과정','-','ty241666@naver.com','전원.jpg'); 
-INSERT INTO `members` VALUES (31,'필웅후','Phil Woong-Hoo','해양융합기술공학','석사과정','-','bixionghou960410@naver.com','필웅후.jpg'); 
+INSERT INTO `members` VALUES (27,'김도훈','Do-hoon Kim','해양융합기술공학','석사과정','-','ehgns320@naver.com','김도훈.jpg');
+INSERT INTO `members` VALUES (28,'정해지','Jeong Hae Ji','해양융합기술공학','석사과정','-','liuhaodong1314@naver.com','정해지.jpg'); 
+INSERT INTO `members` VALUES (29,'왕비','Wang Bee','해양융합기술공학','석사과정','-','wf765969609@gmail.com','왕비.jpg'); 
+INSERT INTO `members` VALUES (30,'장요','Jang yo','해양융합기술공학','석사과정','-','zhang5163365@gmail.com','장요.jpg'); 
+INSERT INTO `members` VALUES (31,'전원','Jun Won ','해양융합기술공학','석사과정','-','ty241666@naver.com','전원.jpg'); 
+INSERT INTO `members` VALUES (32,'필웅후','Phil Woong-Hoo','해양융합기술공학','석사과정','-','bixionghou960410@naver.com','필웅후.jpg'); 
 
 
 
